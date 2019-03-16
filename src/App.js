76dd60568e4b23d4aed3e9aa8816a9026711cb56
Lee Opperman <lee.opperman@hetzner.co.za>
2019-03-16 14:18:00 +0200

    add bootstrap 4 to project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. This will be converted to a login site.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+           <h1>The login page</h1>
+
+            <div className="container">
+                <div className="row">
+                  <div className="col-2">
+                    <label for="username">Username:</label>
+                    <input value="username" ></input>
+                  </div>
+                </div>
+            </div>
+
+            <div >
+              <label for="password" type="password">Password:</label>
+              <input value="password"></input>
+            </div>
+        </div>
       </div>
     );
   }
